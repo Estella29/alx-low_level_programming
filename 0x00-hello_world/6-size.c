3ba1fa821f5d546d282f2b00e0c47aef00524789
Estella29 <estherabunimye@gmail.com>
2023-03-16 14:50:51 -0700

    Sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char: %c byte(s)\n", sizeof(char*));
+	printf("Size of an int: %d byte(s)\n", sizeof(int*));
+	printf("Size of a long int: %ld bytes(s)\n", sizeof(long int*));
+	printf("Size of a long long int: %lld bytes(s)\n", sizeof(long long int*))
+	printf("Size of a float:: %f byte(s)\n", sizeof(float*))
+	return (0)
+}
+

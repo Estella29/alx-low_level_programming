78e1a0c3448cf9a73905a80253904127764b58b9
Estella29 <estherabunimye@gmail.com>
2023-03-17 17:47:49 -0700

    prints all single digit numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - prints the alphabet in lowercase, followed by a new line.
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	while (i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+

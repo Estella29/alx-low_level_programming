85c71ff56f81e795ac03d8b013a53315760d96d3
Estella29 <estherabunimye@gmail.com>
2023-03-21 16:35:10 -0700

    prints the 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,13 +11,15 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		_putchar(0);
+		_putchar('0');
+
 	for (y = 0; y <= 9; y++)
 	{
 		_putchar(',');
 		_putchar(' ');
 
 		z = x * y;
+
 		if (z <= 9)
 			_putchar(' ');
 		else

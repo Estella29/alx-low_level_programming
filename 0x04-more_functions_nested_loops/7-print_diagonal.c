cf8e79dc9c5b7f8c9e34af95716e1dca34f24aba
Estella29 <estherabunimye@gmail.com>
2023-03-23 12:48:25 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line in the terminal
+ *@n: number to print
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+		_putchar('\n');
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < a; b++)
+			{
+			_putchar(' ');
+		}
+
+		_putchar(92);
+		_putchar('\n');
+		}
+	}
+}

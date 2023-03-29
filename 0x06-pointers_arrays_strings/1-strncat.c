3dfc87e3fca52e57437da1a8fc00ed2e5d2a0965
Estella29 <estherabunimye@gmail.com>
2023-03-29 14:43:37 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,15 +9,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
+	int i = 0, j;
 
 	while (dest[i])
 		i++;
 
 	for (j = 0; j < n && src[j] != '\0'; j++)
-	{
+
+		dest[i + j] = src[j];
+
 			dest[i + j] = '\0';
-	}
 
 	return (dest);
 }

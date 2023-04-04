dddc271e44d2a9c9ee2c5f56fc29aa6a2fa862e9
Estella29 <estherabunimye@gmail.com>
2023-04-04 03:10:06 -0700

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ **_strstr- locates a substring.
+ *@haystack: string
+ *
+ *@needle: substring
+ *
+ * Return: output
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i = 0;
+	int j = 0;
+
+	while ((haystack[j] != '\0' || needle[i] != '\0'))
+	{
+		if (needle[i] == haystack[j])
+		{
+			j++;
+			i++;
+		}
+
+			if (needle[i] != '\0')
+			{
+				return (needle);
+				i++;
+			}
+	}
+	return (NULL);
+}
+
